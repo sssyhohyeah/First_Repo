bea8c1bdb3514fc16c01d8ec6c5d0c9c16538e7c
syh <253569556@qq.com>
2019-05-22 11:17:18 +0800

    add read_db

---
+++ read_db.py
@@ -0,0 +1,31 @@
+'''
+    数据库读操作演示（查找select）
+'''
+
+import pymysql
+
+db = pymysql.connect(host="localhost",
+                     port=3306,
+                     user="root",
+                     password="123456",
+                     database="stu",
+                     charset="utf8"
+                     )
+
+c = db.cursor()
+
+sql = "select * from myclass where age=13;"
+
+c.execute(sql)
+
+print(c.fetchone())
+print()
+print(c.fetchmany(3))
+print()
+
+# c.execute(sql)
+print(c.fetchall())
+print()
+print(c.fetchone())
+
+db.close()
